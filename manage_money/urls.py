de5492173b575198068bf4de8450bbc5b8297971
RobinGunarathna <madgunrob@gmail.com>
2022-03-12 03:28:41 +0000

    last sad commit before turning in incomplete project im stuck can't get site to work right now

--- manage_money/urls.py
+++ manage_money/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('',views.index, name="manage")
-    path('add-expense',views.add_expense, name="add-expenses")
+    path('',views.index, name="expenses"),
+    path('add-expense', views.add_expense, name="add-expenses")
 ]
--- money_management/settings.py
+++ money_management/settings.py
@@ -17,8 +17,7 @@ if os.path.isfile('env.py'):
     import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -127,8 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'money_management/static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(Path(__file__).resolve().parent.parent, 'money_management/static')]
+STATIC_ROOT = os.path.join(Path(__file__).resolve().parent.parent, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
